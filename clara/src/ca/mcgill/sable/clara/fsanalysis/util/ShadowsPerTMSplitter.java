af4cf2eb8e66ae7ac19b4f6504c09eec208f0d55
eric.bodden <eric.bodden@ccda6e0a-97a9-11de-8bb7-81ed9af7660d>
2009-09-09 12:14:21 +0000

    license

--- clara/src/ca/mcgill/sable/clara/fsanalysis/util/ShadowsPerTMSplitter.java
+++ clara/src/ca/mcgill/sable/clara/fsanalysis/util/ShadowsPerTMSplitter.java
@@ -1,12 +1,15 @@
-/* abc - The AspectBench Compiler
- * Copyright (C) 2007 Eric Bodden
+/* Clara - Compile-time Approximation of Runtime Analyses
+ * Copyright (C) 2009 Eric Bodden
+ * 
+ * This framework uses technology from Soot, abc, JastAdd and
+ * others. 
  *
- * This compiler is free software; you can redistribute it and/or
+ * This framework is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * This compiler is distributed in the hope that it will be useful,
+ * This framework is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
